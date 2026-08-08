e63393c5fcf22ec4be31a640ad2d0a22c8061f9d
adish-v <93092830+adish-v@users.noreply.github.com>
2026-08-09 01:45:08 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        str1 = {}
+        str2 = {}
+
+        if len(s) != len(t):
+            return False
+
+        for val1, val2 in zip(s, t):
+            str1[val1] = ''
+            str2[val2] = ''
+        
+        if str1 == str2:
+            return True
+        
+        return False
